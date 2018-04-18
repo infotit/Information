8ffd3993bbba4c11f7fb00f75ee9424e6de22cb1
xlwang <infoit7@163.com>
2018-04-18 22:44:03 +0800

    添加业务逻辑模块文件夹modules，注册蓝图

--- info/__init__.py
+++ info/__init__.py
@@ -38,4 +38,8 @@ def create_app(config_name):
 
     CSRFProtect(app)
     Session(app)
+
+    from info.modules.index import index_blu
+    app.register_blueprint(index_blu)
     return app
+
--- manage.py
+++ manage.py
@@ -3,19 +3,12 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db
 
-
 app = create_app("development")
 
 manager = Manager(app)
+
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
-
-@app.route('/')
-def index():
-    session["name"] = "itheima"
-    return 'index33333222223'
-
 
 if __name__ == '__main__':
     manager.run()

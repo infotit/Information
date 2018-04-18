053f62beb548ad0ee129cfd6c699b3a5be19b73a
xlwang <infoit7@163.com>
2018-04-18 22:20:16 +0800

    增加日志

--- info/__init__.py
+++ info/__init__.py
@@ -1,4 +1,6 @@
+from logging.handlers import RotatingFileHandler
 from flask import Flask
+import logging
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
@@ -11,7 +13,21 @@ redis_store = None  # type: StrictRedis
 db = SQLAlchemy()
 
 
+def setup_log(config_name):
+    # 设置日志的记录等级
+    logging.basicConfig(level=config[config_name].LOG_LEVEL)  # 调试debug级
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
+
+
 def create_app(config_name):
+    setup_log(config_name)
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])

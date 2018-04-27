dd68be799ffe61b1af9b2bed123b01edd5bad1a4
xlwang <infoit7@163.com>
2018-04-27 18:06:20 +0800

    bug: 管理员登录后，退出，session中的is_admin未清除，普通用户登录后，可以登录管理页面。 解决方案：用户退出后，清除session中的is_admin

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,5 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, session, redirect, request
 
 admin_blu = Blueprint('admin', __name__)
 
 from . import views
+
+
+@admin_blu.before_request
+def check_admin():
+    is_admin = session.get('is_admin', False)
+    if not is_admin and request.url.endswith('/admin/login'):
+        return redirect('/')
